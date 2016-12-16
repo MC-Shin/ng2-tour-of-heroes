e519964c5f5828477bdd1cb7f61ece95834526b3
S Lalna <S Lalna>
2016-12-16 18:15:34 +0900

    multiple components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Hero} from './heroes/hero';
+import {HEROES} from './mock/mock-heroes';
+
 @Component({
   selector: 'my-app',
   template:`
@@ -64,7 +67,7 @@ import { Component } from '@angular/core';
                 top: -4px;
                 height: 1.8em;
                 margin-right: .8em;
-                border-radius: 4px 0 0 4px;
+                border-radius: 10px 0 0 10px;
               }
             `]          
 })
@@ -79,26 +82,3 @@ export class AppComponent {
     this.selectedHero = hero;
   }
 }
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-
-
-
-
