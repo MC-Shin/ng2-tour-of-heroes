2711b7f47c4c349521b880ef2c4daa7ef5390bfb
boo <mcshin@alticast.com>
2016-12-23 18:21:45 +0900

    ADD 6.Routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,6 +1,6 @@
+import { Hero } from './heroes/hero';
+import { HEROES } from './mock/mock-heroes';
 import { Injectable } from '@angular/core';
-import {Hero} from './heroes/hero'
-import {HEROES} from './mock/mock-heroes'
 
 @Injectable()
 export class HeroService {
@@ -9,8 +9,21 @@ export class HeroService {
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
-    // delay 2 seconds
-    return new Promise<Hero[]>(resolve => setTimeout(resolve, 2000)) .then(() => this.getHeroes());
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
 
-}
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
